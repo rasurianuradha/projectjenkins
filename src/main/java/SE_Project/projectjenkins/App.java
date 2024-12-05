a52c0e9b1a37ad6d6c266e1f7a48a20d16562f11
rasurianuradha <anu057821@gmail.com>
2024-12-05 15:30:51 +0530

    Demo Java

---
+++ src/main/java/SE_Project/projectjenkins/App.java
@@ -0,0 +1,13 @@
+package SE_Project.projectjenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
